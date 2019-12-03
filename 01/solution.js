3895dc09dbd7302c5d20e6dfd814e9b52fdd0614
jordanvidrine <jordan@jordanvidrine.com>
2019-12-03 13:56:25 -0600

    Day 1 Part 1 & 2 - had an epiphany! - i was not accounting for negative integers so I replaced any negative integer with 0

--- 01/solution.js
+++ 01/solution.js
@@ -2,14 +2,26 @@ const fs = require('fs')
 
 const getInput = () => {
   let input = fs.readFileSync('./input.txt')
-  return input.toString().split('\n').map(numberStr => Number(numberStr))
+  return input.toString().split(/\r?\n/).map(numberStr => Number(numberStr))
 }
 
 const calcFuel = (mass) => {
-  return Math.floor(mass/3) -2
+  return Math.floor(mass/3) - 2 > 0 ? Math.floor(mass/3) - 2 : 0
 }
 
-async function solve() {
+const calcFuelForFuel = (mass) => {
+  let totalFuel = 0;
+  // initialize additionalFuel
+  let additionalFuel = Math.floor(mass/3) - 2
+
+  while (additionalFuel >= 0) {
+    totalFuel += additionalFuel;
+    additionalFuel = Math.floor(additionalFuel/3) - 2;
+  }
+  return totalFuel;
+}
+
+function solve() {
   const input = getInput();
 
   const sum = input.reduce((acc, cur) => {
@@ -20,4 +32,19 @@ async function solve() {
   console.log('Answer', sum)
 }
 
+function solvePartTwo() {
+  const input = getInput();
+
+  const sum = input.reduce((acc,cur) => {
+    acc += calcFuelForFuel(cur);
+    return acc;
+  }, 0)
+
+  console.log('Answer', sum)
+}
+
+// part 1
 solve()
+
+// part 2
+solvePartTwo()
---
+++ 03/solution.js
@@ -0,0 +1,16 @@
+// prompt - https://adventofcode.com/2019/day/3
+
+const fs = require('fs')
+
+const getInput = () => {
+  let input = fs.readFileSync('./input.txt')
+  input = input.toString().split(/\r?\n/)
+  let line1 = input[0].split(',')
+  let line2 = input[1].split(',')
+  return {
+    line1,
+    line2
+  }
+}
+
+console.table(getInput())
