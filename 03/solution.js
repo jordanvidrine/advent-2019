5a8872bb877aed211003cd936b2072b67cd07f27
Jordan Vidrine <jordan@jordanvidrine.com>
2019-12-05 14:07:59 -0600

    Day 3 Pt 2 Solution - cant solve, I am assuming theire is a syntax error somewhere. My code works for all examples

--- 03/solution.js
+++ 03/solution.js
@@ -18,6 +18,9 @@ function solve() {
     let line1 = getInput()['line1']
     let line2 = getInput()['line2']
 
+    // let line1 = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51'.split(',')
+    // let line2 = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'.split(',')
+
     let line1Directions = line1.map((instruction)=>{
         let direction = instruction.slice(0, 1)
         let distance = instruction.split(/[RLUD]/)[1]
@@ -62,12 +65,16 @@ function solve() {
         if (set.direction === 'R' || set.direction === 'L') {
             for (let i = 0; i < Number(set.distance); i++) {
                 line1X += vals[set.direction];
-                line1Coords.set(`X${line1X}Y${line1Y}`,line1Coords.size)
+                if (!line1Coords.has(`X${line1X}Y${line1Y}`)) {
+                  line1Coords.set(`X${line1X}Y${line1Y}`,line1Coords.size)
+                }
             }
         } else {
             for (let i = 0; i < Number(set.distance); i++) {
-                line1Y += vals[set.direction];
+              line1Y += vals[set.direction];
+              if (!line1Coords.has(`X${line1X}Y${line1Y}`)) {
                 line1Coords.set(`X${line1X}Y${line1Y}`,line1Coords.size)
+              }
             }
         }
     }
@@ -77,20 +84,32 @@ function solve() {
         if (set.direction === 'R' || set.direction === 'L') {
             for (let i = 0; i < Number(set.distance); i++) {
               line2X += vals[set.direction];
-              line2Coords.set(`X${line2X}Y${line2Y}`,line2Coords.size)
+              if (!line2Coords.has(`X${line2X}Y${line2Y}`)) {
+                line2Coords.set(`X${line2X}Y${line2Y}`,line2Coords.size)
+              }
             }
         } else {
             for (let i = 0; i < Number(set.distance); i++) {
-                line2Y += vals[set.direction];
+              line2Y += vals[set.direction];
+              if (!line2Coords.has(`X${line2X}Y${line2Y}`)) {
                 line2Coords.set(`X${line2X}Y${line2Y}`,line2Coords.size)
+              }
             }
         }
     }
     )
 
-    let intersections = getIntersections(line1Coords, line2Coords)
+    // Part One
+    // let intersections = getIntersections(line1Coords, line2Coords)
+    // return getShortestManhattanDistance(intersections)
 
-    return getShortestManhattanDistance(intersections)
+    // Part Two
+    let intersections = getIntersections(line1Coords, line2Coords)
+    return intersections.reduce((acc,cur) => {
+      let distance = line1Coords.get(cur)+1 + line2Coords.get(cur)+1
+      if (distance < acc) return distance;
+      return acc;
+    }, Infinity)
 }
 
 function getIntersections(line1, line2) {
@@ -109,7 +128,7 @@ function getShortestManhattanDistance(intersections) {
     let parsedString = cur.split(/[XY]/)
     let X = parsedString[parsedString.length-2]
     let Y = parsedString[parsedString.length-1]
-    let manhattanDistance = Math.abs(Number(X)) + Math.abs(Number(Y))
+    let manhattanDistance = Math.abs(0 - Number(X)) + Math.abs(0 - Number(Y))
     if (manhattanDistance < acc) return manhattanDistance;
     return acc;
   },Infinity)
